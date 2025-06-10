5e4a0354e964eae4716b135707846638f9550b21
GenuineJack <6517070+GenuineJack@users.noreply.github.com>
2025-06-10 19:01:30 -0400

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LostTimer from './LostTimer';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LostTimer />
+  </React.StrictMode>
+);
